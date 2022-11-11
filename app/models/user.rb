04359f3d4dd4e764cd6901efba74e2e08bb83b21
Matti Lavikainen <matti.lavikainen@helsinki.fi>
2022-11-11 16:49:04 +0200

    Week 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
 
   has_secure_password
   has_many :ratings, dependent: :destroy
+  has_many :memberships, dependent: :destroy
   has_many :beers, through: :ratings
+  has_many :beer_clubs, through: :memberships
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
   validates :password, format: { with: /(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{4,}/, message: "must be at least 4 characters and include capital letter and one number." }
