4ecd7eb2a7e2fe1ed5d9d8047ce0177608044388
Matti Lavikainen <matti.lavikainen@helsinki.fi>
2022-12-30 20:14:27 +0200

    session controller fix from last week

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by username: params[:username]
-    if user.closed?
-      redirect_to signin_path, notice: "Your account is closed, please contact with admin"
-    elsif user&.authenticate(params[:password])
+
+    if user.nil? || !user.authenticate(params[:password])
+      redirect_to signin_path, notice: "Username and/or password mismatch"
+    elsif user.closed?
+      redirect_to signin_path, notice: "account closed, please contact admin"
+    else
       session[:user_id] = user.id
       redirect_to user_path(user), notice: "Welcome back!"
-    else
-      redirect_to signin_path, notice: "Username and/or password mismatch"
     end
   end
 
